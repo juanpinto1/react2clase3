d6ca94ccc7bc0601b9879bdfb722c85765b04428
juanpinto1 <juan.pinto@cajalosandes.cl>
2024-03-04 01:16:52 -0300

    poke4

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,11 @@
+import pikachu from "../img/pikachu.png";
 const Home = () => {
   return (
     <div className="mt-5 text-center">
       <h1>Bienvenido maestro pokemón</h1>
       <img
         height="300"
-        src="../../public/pikachu.png"
+        src={pikachu}
         alt=""
       />
     </div>
