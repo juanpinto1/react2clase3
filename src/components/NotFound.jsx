5d2d4d76ad0adc9e5ccaa85de52752c1909c4e58
juanpinto1 <juan.pinto@cajalosandes.cl>
2024-03-04 00:48:57 -0300

    hola5

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,9 +1,11 @@
 import { Container } from "react-bootstrap";
+import "./PokemonCardstyle.css";
 
 const NotFound = () => {
 return (
     <Container className="pt-5">
     <h1 className="mb-4">La ruta que intentas consultar no existe 😑</h1>
+    <button className="btn1" onClick={returnHome}> Volver al Inicio</button>
     </Container>
 );
 };
