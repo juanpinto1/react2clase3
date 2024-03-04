9327204be58f680ec32ec56549fd59ab8cd02f58
juanpinto1 <juan.pinto@cajalosandes.cl>
2024-03-04 00:58:28 -0300

    hola8

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,9 +3,10 @@ import { useNavigate } from "react-router-dom";
 import "./PokemonCardstyle.css";
 
 const NotFound = () => {
+    const navigate1 = useNavigate();
     const returnHome1 = () => {
-        navigate("/");
-      };
+        navigate1("/");
+    };
 return (
     <Container className="pt-5">
     <h1 className="mb-4">La ruta que intentas consultar no existe 😑</h1>
